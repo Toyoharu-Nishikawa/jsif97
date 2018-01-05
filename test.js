a4e70cb3662c7c6731dcff1921746fd08c210281
toyoharu nishikawa <toyohal@email.com>
2018-01-05 20:11:45 +0900

    add IF97_3.js

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 import {region_1} from './IF97_1.js'
 import {region_2} from './IF97_2.js'
+import {region_3} from './IF97_3.js'
 
 var SP= {
-  P: 1,
+  v: 1,
   T: 500,
 };
-region_2(SP)
+region_3(SP)
 console.log(SP)
